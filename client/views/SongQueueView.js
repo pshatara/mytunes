bcab840e81d44452170903296fe293fc5d2a5464
pshatara <pshatara@gmail.com>
2014-09-24 19:10:18 -0700

    Add song to playlist when clicked and appends to DOM.

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,23 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagname: 'table',
+
   initialize: function() {
+    this.collection.on('add remove', function() {
+      this.render();
+    }, this);
   },
 
   render: function() {
-    return this.$el;
+    console.log('test')
+    this.$el.children().detach();
+
+    this.$el.html('<th>Play List</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
